acdaf88c51263507f383d77cc5436de4dbc136c2
swamfire <usern5949@gmail.com>
2023-08-30 01:57:17 +0530

    js vars

--- Fundamentals Part 1/script.js
+++ Fundamentals Part 1/script.js
@@ -1,3 +1,31 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!!!");
-console.log(40 + 8 + 23 - 11)
+// if (js === "amazing") alert("JavaScript is FUN!!!");
+// console.log(40 + 8 + 23 - 11)
+
+// console.log("Swamfire")
+// console.log(23)
+
+let firstName = "Swamfire"
+// // alert(firstName)
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+
+let first_name = "SwamFire"
+
+let $first_name = 'SwamFire'
+
+// console.log(first_name)
+// console.log($first_name)
+
+
+// Always use descriptive names for your vars
+
+
+// Assignment
+let country = "INDIA"
+let continent = "Asea"
+let population = 12345
+console.log(country, continent, population)
+
+
