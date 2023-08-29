09d835c6cd5bf49bd7f226dbf4619f8a9f78108d
swamfire <usern5949@gmail.com>
2023-08-30 02:14:05 +0530

    added js datatypes

--- Fundamentals Part 1/script.js
+++ Fundamentals Part 1/script.js
@@ -1,7 +1,6 @@
 let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!!!");
 // console.log(40 + 8 + 23 - 11)
-
 // console.log("Swamfire")
 // console.log(23)
 
@@ -10,22 +9,49 @@ let firstName = "Swamfire"
 // console.log(firstName)
 // console.log(firstName)
 // console.log(firstName)
-
 let first_name = "SwamFire"
-
 let $first_name = 'SwamFire'
-
 // console.log(first_name)
 // console.log($first_name)
 
 
 // Always use descriptive names for your vars
 
-
+/*
 // Assignment
 let country = "INDIA"
 let continent = "Asea"
 let population = 12345
 console.log(country, continent, population)
+*/
 
+/*
+Primitive Datatypes
+1. Number
+2. Strings
+3. Booleans
+4. null
+5. undefined
+6. Symbol
+7. BigInt
+*/
+
+// console.log(typeof 1)
+// console.log(typeof "SwamFire")
+// console.log(typeof true)
+// console.log(true)
+let JavaScriptIsFun = true
+// console.log(JavaScriptIsFun)
+// console.log(typeof JavaScriptIsFun)
+JavaScriptIsFun = 1
+// console.log(JavaScriptIsFun)
+// console.log(typeof JavaScriptIsFun)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 2023
+console.log(year)
+console.log(typeof year)
 
