b1d70ba0253f848af3c0dcbfb67952915c4da88f
swamfire <usern5949@gmail.com>
2023-09-02 00:23:20 +0530

    Added functions

--- Fundamentals Part 2/script.js
+++ Fundamentals Part 2/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false
 const passTest = true
 if (passTest) {
@@ -8,3 +9,29 @@ if (passTest) {
 if (hasDriversLicense) {
     console.log("I can Drive")
 }
+*/
+
+// DRY -> Dont Repeat Yourself
+function logger() {
+    console.log('logger')  // this is a function that will be called in the next line of code
+}
+
+//invoking function or calling the function or running the function
+logger()
+console.log(logger()) // function without any return vars will return undefined
+logger()
+
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+let appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
+console.log(fruitProcessor(5, 0))
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
+
+console.log(Number('23'))
